82d5b17263448aad0ff442242811a9f6faa73b73
Andreas Madsack <andreas@madflex.de>
2020-02-21 22:24:10 +0100

    add evaluation script
    pretty slow on raspberry but works
    slow because of line by line json reading

---
+++ evaluate.py
@@ -0,0 +1,33 @@
+import json
+import click
+import numpy as np
+import scipy.signal
+from pathlib import Path
+
+
+def load(fn):
+    with open(fn) as fp:
+        for line in fp.readlines():
+            d = json.loads(line)
+            yield (d.get("xAcc_mma7455"))
+
+
+def get_peaks(x):
+    _ = np.negative(x)
+    _ = scipy.signal.savgol_filter(_, 51, 3)
+    peaks, properties = scipy.signal.find_peaks(_, prominence=4, width=40)
+    return sum(map(lambda i: i > 10, properties["prominences"]))
+
+
+@click.command()
+@click.option("--filename", default="", help="use newest if empty")
+def main(filename):
+    if not filename:
+        # get newest file in folder
+        filename = max((f.stat().st_mtime, f) for f in Path("logs").iterdir())[1]
+
+    print("peaks:", get_peaks(list(load(filename))))
+
+
+if __name__ == "__main__":
+    main()
